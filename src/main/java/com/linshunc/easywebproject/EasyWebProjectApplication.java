ca43572ea30140516cc87e2122b303716a2797ca
chxn-lin <chxn_lin@163.com>
2023-10-29 14:42:44 +0800

    first commit

---
+++ src/main/java/com/linshunc/easywebproject/EasyWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.linshunc.easywebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyWebProjectApplication.class, args);
+    }
+
+}
